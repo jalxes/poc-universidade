963e80bf91c187e8ec6439f47acb97f344cf9c8d
Jefferson Silveira <devjeffersonsilveira@gmail.com>
2017-08-29 23:35:54 -0300

    foi

--- scripts/rascunho.sql
+++ scripts/rascunho.sql
@@ -1,130 +1,65 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema db
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `db` ;
+
+-- -----------------------------------------------------
+-- Schema db
+-- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `db` DEFAULT CHARACTER SET latin1 ;
 USE `db` ;
 
 -- -----------------------------------------------------
--- Table `db`.`curso`
+-- Table `db`.`instituicao`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `db`.`curso` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(255) NULL DEFAULT NULL,
+DROP TABLE IF EXISTS `db`.`instituicao` ;
+
+CREATE TABLE IF NOT EXISTS `db`.`instituicao` (
+  `id` INT NOT NULL,
+  `nome` VARCHAR(45) NULL,
   PRIMARY KEY (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = latin1;
+ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `db`.`aluno`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `db`.`aluno` ;
+
 CREATE TABLE IF NOT EXISTS `db`.`aluno` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(255) NULL DEFAULT NULL,
-  `curso_id` INT(11) NOT NULL,
-  PRIMARY KEY (`id`, `curso_id`),
-  INDEX `fk_aluno_curso1_idx` (`curso_id` ASC),
-  CONSTRAINT `fk_aluno_curso1`
-    FOREIGN KEY (`curso_id`)
-    REFERENCES `db`.`curso` (`id`)
+  `instituicao_id` INT NOT NULL,
+  PRIMARY KEY (`id`, `instituicao_id`),
+  CONSTRAINT `fk_aluno_instituicao1`
+    FOREIGN KEY (`instituicao_id`)
+    REFERENCES `db`.`instituicao` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = latin1;
 
-
--- -----------------------------------------------------
--- Table `db`.`professor`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `db`.`professor` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(255) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = latin1;
+CREATE INDEX `fk_aluno_instituicao1_idx` ON `db`.`aluno` (`instituicao_id` ASC);
 
 
 -- -----------------------------------------------------
 -- Table `db`.`disciplina`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `db`.`disciplina` ;
+
 CREATE TABLE IF NOT EXISTS `db`.`disciplina` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(255) NULL DEFAULT NULL,
-  `professor_id` INT(11) NOT NULL,
-  PRIMARY KEY (`id`, `professor_id`),
-  INDEX `fk_disciplina_professor1_idx` (`professor_id` ASC),
-  CONSTRAINT `fk_disciplina_professor1`
-    FOREIGN KEY (`professor_id`)
-    REFERENCES `db`.`professor` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = latin1;
-
-
--- -----------------------------------------------------
--- Table `db`.`faltas`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `db`.`faltas` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(255) NULL DEFAULT NULL,
-  `aluno_id` INT(11) NOT NULL,
-  PRIMARY KEY (`id`, `aluno_id`),
-  INDEX `fk_faltas_aluno1_idx` (`aluno_id` ASC),
-  CONSTRAINT `fk_faltas_aluno1`
-    FOREIGN KEY (`aluno_id`)
-    REFERENCES `db`.`aluno` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = latin1;
-
-
--- -----------------------------------------------------
--- Table `db`.`matricula`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `db`.`matricula` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(255) NULL DEFAULT NULL,
   PRIMARY KEY (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = latin1;
-
-
--- -----------------------------------------------------
--- Table `db`.`notas`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `db`.`notas` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(255) NULL DEFAULT NULL,
-  `aluno_id` INT(11) NOT NULL,
-  PRIMARY KEY (`id`, `aluno_id`),
-  INDEX `fk_notas_aluno1_idx` (`aluno_id` ASC),
-  CONSTRAINT `fk_notas_aluno1`
-    FOREIGN KEY (`aluno_id`)
-    REFERENCES `db`.`aluno` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = latin1;
-
-
--- -----------------------------------------------------
--- Table `db`.`disciplina_has_curso`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `db`.`disciplina_has_curso` (
-  `disciplina_id` INT(11) NOT NULL,
-  `curso_id` INT(11) NOT NULL,
-  PRIMARY KEY (`disciplina_id`, `curso_id`),
-  INDEX `fk_disciplina_has_curso_curso1_idx` (`curso_id` ASC),
-  INDEX `fk_disciplina_has_curso_disciplina_idx` (`disciplina_id` ASC),
-  CONSTRAINT `fk_disciplina_has_curso_disciplina`
-    FOREIGN KEY (`disciplina_id`)
-    REFERENCES `db`.`disciplina` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_disciplina_has_curso_curso1`
-    FOREIGN KEY (`curso_id`)
-    REFERENCES `db`.`curso` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = latin1;
 
@@ -132,12 +67,12 @@ DEFAULT CHARACTER SET = latin1;
 -- -----------------------------------------------------
 -- Table `db`.`aluno_has_disciplina`
 -- -----------------------------------------------------
+DROP TABLE IF EXISTS `db`.`aluno_has_disciplina` ;
+
 CREATE TABLE IF NOT EXISTS `db`.`aluno_has_disciplina` (
   `aluno_id` INT(11) NOT NULL,
   `disciplina_id` INT(11) NOT NULL,
   PRIMARY KEY (`aluno_id`, `disciplina_id`),
-  INDEX `fk_aluno_has_disciplina_disciplina1_idx` (`disciplina_id` ASC),
-  INDEX `fk_aluno_has_disciplina_aluno1_idx` (`aluno_id` ASC),
   CONSTRAINT `fk_aluno_has_disciplina_aluno1`
     FOREIGN KEY (`aluno_id`)
     REFERENCES `db`.`aluno` (`id`)
@@ -150,6 +85,171 @@ CREATE TABLE IF NOT EXISTS `db`.`aluno_has_disciplina` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = latin1;
+
+CREATE INDEX `fk_aluno_has_disciplina_disciplina1_idx` ON `db`.`aluno_has_disciplina` (`disciplina_id` ASC);
+
+CREATE INDEX `fk_aluno_has_disciplina_aluno1_idx` ON `db`.`aluno_has_disciplina` (`aluno_id` ASC);
+
+
+-- -----------------------------------------------------
+-- Table `db`.`curso`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `db`.`curso` ;
+
+CREATE TABLE IF NOT EXISTS `db`.`curso` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NULL DEFAULT NULL,
+  `instituicao_id` INT NOT NULL,
+  PRIMARY KEY (`id`, `instituicao_id`),
+  CONSTRAINT `fk_curso_instituicao1`
+    FOREIGN KEY (`instituicao_id`)
+    REFERENCES `db`.`instituicao` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
+
+CREATE INDEX `fk_curso_instituicao1_idx` ON `db`.`curso` (`instituicao_id` ASC);
+
+
+-- -----------------------------------------------------
+-- Table `db`.`disciplina_has_curso`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `db`.`disciplina_has_curso` ;
+
+CREATE TABLE IF NOT EXISTS `db`.`disciplina_has_curso` (
+  `disciplina_id` INT(11) NOT NULL,
+  `curso_id` INT(11) NOT NULL,
+  PRIMARY KEY (`disciplina_id`, `curso_id`),
+  CONSTRAINT `fk_disciplina_has_curso_curso1`
+    FOREIGN KEY (`curso_id`)
+    REFERENCES `db`.`curso` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_disciplina_has_curso_disciplina`
+    FOREIGN KEY (`disciplina_id`)
+    REFERENCES `db`.`disciplina` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
+
+CREATE INDEX `fk_disciplina_has_curso_curso1_idx` ON `db`.`disciplina_has_curso` (`curso_id` ASC);
+
+CREATE INDEX `fk_disciplina_has_curso_disciplina_idx` ON `db`.`disciplina_has_curso` (`disciplina_id` ASC);
+
+
+-- -----------------------------------------------------
+-- Table `db`.`professor`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `db`.`professor` ;
+
+CREATE TABLE IF NOT EXISTS `db`.`professor` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NULL DEFAULT NULL,
+  `curso_id` INT(11) NOT NULL,
+  `instituicao_id` INT NOT NULL,
+  PRIMARY KEY (`id`, `curso_id`, `instituicao_id`),
+  CONSTRAINT `fk_professor_curso1`
+    FOREIGN KEY (`curso_id`)
+    REFERENCES `db`.`curso` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_professor_instituicao1`
+    FOREIGN KEY (`instituicao_id`)
+    REFERENCES `db`.`instituicao` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
+
+CREATE INDEX `fk_professor_curso1_idx` ON `db`.`professor` (`curso_id` ASC);
+
+CREATE INDEX `fk_professor_instituicao1_idx` ON `db`.`professor` (`instituicao_id` ASC);
+
+
+-- -----------------------------------------------------
+-- Table `db`.`faltas`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `db`.`faltas` ;
+
+CREATE TABLE IF NOT EXISTS `db`.`faltas` (
+  `aluno_id` INT(11) NOT NULL,
+  `disciplina_id` INT(11) NOT NULL,
+  `faltascol` DATETIME NOT NULL,
+  PRIMARY KEY (`aluno_id`, `disciplina_id`, `faltascol`),
+  CONSTRAINT `fk_aluno_has_disciplina1_aluno1`
+    FOREIGN KEY (`aluno_id`)
+    REFERENCES `db`.`aluno` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_aluno_has_disciplina1_disciplina1`
+    FOREIGN KEY (`disciplina_id`)
+    REFERENCES `db`.`disciplina` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
+
+CREATE INDEX `fk_aluno_has_disciplina1_disciplina1_idx` ON `db`.`faltas` (`disciplina_id` ASC);
+
+CREATE INDEX `fk_aluno_has_disciplina1_aluno1_idx` ON `db`.`faltas` (`aluno_id` ASC);
+
+
+-- -----------------------------------------------------
+-- Table `db`.`nota`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `db`.`nota` ;
+
+CREATE TABLE IF NOT EXISTS `db`.`nota` (
+  `aluno_id` INT(11) NOT NULL,
+  `disciplina_id` INT(11) NOT NULL,
+  `nota` FLOAT NOT NULL,
+  `data` DATETIME NOT NULL,
+  PRIMARY KEY (`aluno_id`, `disciplina_id`, `data`, `nota`),
+  CONSTRAINT `fk_aluno_has_disciplina2_aluno1`
+    FOREIGN KEY (`aluno_id`)
+    REFERENCES `db`.`aluno` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_aluno_has_disciplina2_disciplina1`
+    FOREIGN KEY (`disciplina_id`)
+    REFERENCES `db`.`disciplina` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
+
+CREATE INDEX `fk_aluno_has_disciplina2_disciplina1_idx` ON `db`.`nota` (`disciplina_id` ASC);
+
+CREATE INDEX `fk_aluno_has_disciplina2_aluno1_idx` ON `db`.`nota` (`aluno_id` ASC);
+
+
+-- -----------------------------------------------------
+-- Table `db`.`aluno_has_curso`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `db`.`aluno_has_curso` ;
+
+CREATE TABLE IF NOT EXISTS `db`.`aluno_has_curso` (
+  `aluno_id` INT(11) NOT NULL,
+  `curso_id` INT(11) NOT NULL,
+  PRIMARY KEY (`aluno_id`, `curso_id`),
+  CONSTRAINT `fk_aluno_has_curso_aluno1`
+    FOREIGN KEY (`aluno_id`)
+    REFERENCES `db`.`aluno` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_aluno_has_curso_curso1`
+    FOREIGN KEY (`curso_id`)
+    REFERENCES `db`.`curso` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1;
+
+CREATE INDEX `fk_aluno_has_curso_curso1_idx` ON `db`.`aluno_has_curso` (`curso_id` ASC);
+
+CREATE INDEX `fk_aluno_has_curso_aluno1_idx` ON `db`.`aluno_has_curso` (`aluno_id` ASC);
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
